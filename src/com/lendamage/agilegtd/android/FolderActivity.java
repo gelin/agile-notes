646247a13e8a82678679d105c7ba588bcc9f8b1d
Denis Nelubin <dnelubin@gmail.com>
2011-07-31 16:02:55 +0700

    Started folder activity layout...

--- src/com/lendamage/agilegtd/android/FolderActivity.java
+++ src/com/lendamage/agilegtd/android/FolderActivity.java
@@ -2,12 +2,22 @@ package com.lendamage.agilegtd.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
 
 public class FolderActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.folder);
+        setTitle(R.string.app_name);
     }
+    
+    private void setTitle(String title) {
+        TextView titleView = (TextView)findViewById(R.id.title);
+        titleView.setText(title);
+    }
+    
 }
