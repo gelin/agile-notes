75ccb2b6b2496e9c420296844580238b880686e1
Denis Nelubin <dnelubin@gmail.com>
2011-09-20 00:08:17 +0700

    Improved work with soft keyboard.

--- src/com/lendamage/agilegtd/android/AddActionActivity.java
+++ src/com/lendamage/agilegtd/android/AddActionActivity.java
@@ -1,10 +1,6 @@
 package com.lendamage.agilegtd.android;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.View.OnFocusChangeListener;
-import android.widget.TextView;
 
 /**
  *  Activity to add a new action.
@@ -17,17 +13,6 @@ public class AddActionActivity extends AbstractFolderActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_action_activity);
-        
-        TextView body = (TextView)findViewById(R.id.action_body);
-        body.setOnFocusChangeListener(new OnFocusChangeListener() {
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    //http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob;f=core/java/android/preference/DialogPreference.java;h=bbad2b6d432ce44ad05ddbc44487000b150135ef;hb=HEAD
-                    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE |
-                            WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
-                }
-            }
-        });
     }
 
 }
