9a12fc64afecd448f75666b27eafb073fcf63e02
Denis Nelubin <dnelubin@gmail.com>
2011-09-20 00:39:57 +0700

    Trying to find the way to detect appearing/disappearing of the soft keyboard...

--- src/com/lendamage/agilegtd/android/AddActionActivity.java
+++ src/com/lendamage/agilegtd/android/AddActionActivity.java
@@ -1,6 +1,7 @@
 package com.lendamage.agilegtd.android;
 
 import android.os.Bundle;
+import android.view.WindowManager.LayoutParams;
 
 /**
  *  Activity to add a new action.
@@ -13,6 +14,24 @@ public class AddActionActivity extends AbstractFolderActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_action_activity);
+    }
+    
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        System.out.println(this.getClass().getName() + ".onWindowFocusChanged(): " + hasFocus);
+    }
+    
+    @Override
+    public void onWindowAttributesChanged(LayoutParams params) {
+        super.onWindowAttributesChanged(params);
+        System.out.println(this.getClass().getName() + ".onWindowAttributesChanged(): " + params);
+    }
+    
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        System.out.println(this.getClass().getName() + ".onAttachedToWindow()");
     }
 
 }
