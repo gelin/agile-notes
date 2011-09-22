9ff58b24141596e8c09aa10fe0205b7a0a747163
Denis Nelubin <dnelubin@gmail.com>
2011-09-23 00:05:59 +0600

    Updated adapter to display actions.

--- src/com/lendamage/agilegtd/android/FolderListAdapter.java
+++ src/com/lendamage/agilegtd/android/FolderListAdapter.java
@@ -1,5 +1,8 @@
 package com.lendamage.agilegtd.android;
 
+import java.util.List;
+
+import com.lendamage.agilegtd.model.Action;
 import com.lendamage.agilegtd.model.Folder;
 
 import android.content.Context;
@@ -15,58 +18,146 @@ import android.widget.TextView;
  */
 public class FolderListAdapter extends BaseAdapter {
 
+    /** View type of folder */
+    static final int FOLDER_VIEW_TYPE = 0;
+    /** View type of action */
+    static final int ACTION_VIEW_TYPE = 1;
+    /** Number of view types */
+    static final int VIEW_TYPES = 2;
+    
     /** Current context */
     Context context;
     /** Folder over which the adapter works */
     Folder folder;
+    /** List of folders */
+    List<Folder> folders;
+    /** List of actions */
+    List<Action> actions;
     
     public FolderListAdapter(Context context, Folder folder) {
         this.context = context;
         this.folder = folder;
+        update();
+    }
+    
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+    
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPES;
+    }
+    
+    @Override
+    public int getItemViewType(int position) {
+        if (isFolder(position)) {
+            return FOLDER_VIEW_TYPE;
+        } else {
+            return ACTION_VIEW_TYPE;
+        }
     }
     
     public int getCount() {;
-        //TODO: add actions.
-        return folder.getFolders().size();
+        update();
+        return this.folders.size() + this.actions.size();
     }
 
     public Object getItem(int position) {
-        //TODO: add actions
-        return folder.getFolders().get(position);
+        if (isFolder(position)) {
+            return this.folders.get(position);
+        } else {
+            return this.actions.get(toActionPosition(position));
+        }
     }
 
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
-        //TODO: add actions
-        if (view == null) {
-            view = LayoutInflater.from(this.context).inflate(
-                    R.layout.folder_list_item, parent, false);
+        if (isFolder(position)) {
+            if (view == null) {
+                view = inflateFolderView(parent); 
+            }
+            bindFolderView(view, this.folders.get(position));
+        } else {
+            if (view == null) {
+                view = inflateActionView(parent); 
+            }
+            bindActionView(view, this.actions.get(toActionPosition(position)));
         }
-        TextView folderName = (TextView)view.findViewById(R.id.folder_name);
-        folderName.setText(folder.getFolders().get(position).getName());
-        
         return view;
+    }
+    
+    /**
+     *  Updates the folders and actions lists.
+     */
+    void update() {
+        this.folders = this.folder.getFolders();
+        this.actions = this.folder.getActions();
+    }
+    
+    /**
+     *  Checks that the folder (not action) is located at the specified position.
+     */
+    boolean isFolder(int position) {
+        return position < this.folders.size();
+    }
+    
+    /**
+     *  Creates new view for folder.
+     */
+    View inflateFolderView(ViewGroup parent) {
+        return LayoutInflater.from(this.context).inflate(
+                R.layout.folder_list_item, parent, false);
+    }
+    
+    /**
+     *  Binds folder to view.
+     */
+    void bindFolderView(View view, Folder folder) {
+        TextView folderName = (TextView)view.findViewById(R.id.folder_name);
+        folderName.setText(folder.getName());
+    }
+    
+    /**
+     *  Creates new view for action.
+     */
+    View inflateActionView(ViewGroup parent) {
+        return LayoutInflater.from(this.context).inflate(
+                R.layout.action_list_item, parent, false);
+    }
+    
+    /**
+     *  Binds action to view.
+     */
+    void bindActionView(View view, Action action) {
+        TextView actionHead = (TextView)view.findViewById(R.id.action_head);
+        actionHead.setText(action.getHead());
+    }
+    
+    /**
+     *  Converts the list position to the position in the actions list.
+     */
+    int toActionPosition(int position) {
+        return position - this.folders.size();
     }
     
     /**
      *  Checks that the specified position is the position of the first folder.
      */
     boolean isFirstFolder(int position) {
-        //TODO: add actions
-        return position == 0;
+        return isFolder(position) && position == 0;
     }
     
     /**
      *  Checks that the specified position is the position of the last folder.
      */
     boolean isLastFolder(int position) {
-        //TODO: add actions
-        return position == getCount() - 1;
+        return isFolder(position) && position == this.folders.size() - 1;
     }
 
 }
